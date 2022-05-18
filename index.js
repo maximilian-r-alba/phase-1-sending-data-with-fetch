2993c808ef2f374db022d56f54b1c7adb3b1ab1a
Maximilian Alba <albam476@gmail.com>
2022-05-17 22:57:58 -0400

    commit tree da1c47466fb158beae7612d2628e23b3ef70ca1a

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(userName, userEmail){
+
+    const methodObj = { method: "Post", 
+                         headers: {"Content-Type": "application/json", "Accept": "application/json"},
+                        body: JSON.stringify({name : userName, email : userEmail})
+                        
+    }
+    return fetch('http://localhost:3000/users',methodObj)
+    .then(response => response.json())
+    .then(object => {
+            let child = document.querySelector('body').appendChild(document.createElement('p1'))
+            child.textContent = object.id
+    })
+    .catch(error => handleError(error))
+
+}
+
+function handleError(error){
+    let message = document.createElement('p1')
+    message.textContent = error.message
+    document.querySelector('body').appendChild(message)
+}
